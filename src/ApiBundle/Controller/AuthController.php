d984542faa76769aaa74e2e1f4df5f4191c6d134
anonim1133 <anonim1133@wp.pl>
2015-01-02 18:02:17 +0100

    Dodane API a w nim:
    - Logowanie
    - sprawdzanie czy klucz jest ważny
    - rejestrację

--- src/ApiBundle/Controller/AuthController.php
+++ src/ApiBundle/Controller/AuthController.php
@@ -2,31 +2,121 @@
 
 namespace ApiBundle\Controller;
 
+use ApiBundle\Entity\ApiKeys;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use ZacjaBundle\Entity\User;
 
-class AuthController extends Controller
-{
+
+class AuthController extends Controller{
     /**
      * @Route("/api/SignIn")
      * @Template()
      */
-    public function SignInAction()
-    {
-        return array(
-                // ...
-            );    }
+    public function SignInAction(){
+        //Get user credentials
+        $request = Request::createFromGlobals();
+        $username = $request->query->get('login');
+        $password = $request->query->get('password');
+
+        $response = new Response();
+
+        if(isset($username) && strlen($username) > 0 && isset($password) && strlen($password) > 0 ){
+            //Authorize user
+            $em = $this->getDoctrine()->getManager();
+            $user = $em->getRepository('ZacjaBundle:User')->findOneBy(array('username' => $username, 'password' => $password));
+
+            //Create response
+            if(isset($user) && $user->getUsername() == $username){
+                $key = $em->getRepository('ApiBundle:ApiKeys')->findOneBy(array('userId' => $user->getId()));
+
+                if(isset($key) && $key->getApiKey() != 0){
+                    $response->setContent($key->getApiKey());
+                }else{
+
+                    $key = md5('API' . time() . 'KEY_FOR_' . $user->getUsername());
+
+                    $api_key = new ApiKeys();
+
+                    $api_key->setUserId($user->getId());
+                    $api_key->setApikey($key);
+                    $api_key->setDate(time());
+
+                    $em->persist($api_key);
+                    $em->flush();
+
+                    $response->setContent($key);
+                }
+
+                $response->setStatusCode(Response::HTTP_OK);
+            }else{
+                $response->setContent('false');
+                $response->setStatusCode(Response::HTTP_FORBIDDEN);
+            }
+
+            //Generate response
+
+
+            $response->headers->set('Content-Type', 'text/html');
+            // prints the HTTP headers followed by the content
+
+
+        }else{
+            $response->setStatusCode(Response::HTTP_PARTIAL_CONTENT);
+            $response->headers->set('Content-Type', 'text/html');
+            $response->setContent('false');
+        }
+            return $response;
+}
 
     /**
      * @Route("/api/SignUp")
      * @Template()
      */
-    public function signUpAction()
-    {
-        return array(
-                // ...
-            );    }
+    public function signUpAction(){
+        //ToDo: Sprawdzać czy dany user nie istnieje
+        $response = new Response();
+        $em = $this->getDoctrine()->getManager();
+        $request = Request::createFromGlobals();
+
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+        $email = $request->request->get('email');
+
+        if(isset($username) & isset($password) & isset($email) & strlen($username) > 0 & strlen($password) > 0 & strlen($email) > 0){
+            $salt = 'saltedSalt';
+            $isActive = false;
+            $roles = 0;
+            $registrationDate = new \DateTime('now');
+
+            $user = new User();
+            $user->setUsername($username);
+            $user->setPassword($password);
+            $user->setEmail($email);
+            $user->setSalt($salt);
+            $user->setIsActive($isActive);
+            $user->setRoles($roles);
+            $user->setRegistrationDate($registrationDate);
+
+            $em->persist($user);
+            $em->flush();
+
+            $response->headers->set('Content-Type', 'text/html');
+            $response->setContent('true');
+            $response->setStatusCode(Response::HTTP_ACCEPTED);
+
+            return $response;
+        }else{
+            $response->headers->set('Content-Type', 'text/html');
+            $response->setContent('false');
+            $response->setStatusCode(Response::HTTP_BAD_REQUEST);
+
+            return $response;
+        }
+    }
 
     /**
      * @Route("/api/LoginCheck")
@@ -34,9 +124,27 @@ class AuthController extends Controller
      */
     public function CheckAction()
     {
-        return array(
-                // ...
-            );    }
+        $request = Request::createFromGlobals();
+        $key = $request->query->get('key');
+
+        $em = $this->getDoctrine()->getManager();
+        $key = $em->getRepository('ApiBundle:ApiKeys')->findOneBy(array('apikey' => $key));
+
+        $response = new Response();
+
+        if(isset($key)){
+            $response->setContent($key->getUserId());
+            $response->setStatusCode(Response::HTTP_OK);
+        }else{
+            $response->setContent('false');
+            $response->setStatusCode(Response::HTTP_FORBIDDEN);
+        }
+
+        $response->headers->set('Content-Type', 'text/html');
+        // prints the HTTP headers followed by the content
+
+        return $response;
+        }
 
     /**
      * @Route("/api/Confirm")
---
+++ src/ApiBundle/Entity/ApiKeys.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace ApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ApiKeys
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class ApiKeys
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     */
+    private $userId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="apikey", type="string", length=255)
+     */
+    private $apikey;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="date", type="string", length=255)
+     */
+    private $date;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return ApiKeys
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set apikey
+     *
+     * @param string $apikey
+     * @return ApiKeys
+     */
+    public function setApikey($apikey)
+    {
+        $this->apikey = $apikey;
+
+        return $this;
+    }
+
+    /**
+     * Get apikey
+     *
+     * @return string 
+     */
+    public function getApikey()
+    {
+        return $this->apikey;
+    }
+
+    /**
+     * Set date
+     *
+     * @param string $date
+     * @return ApiKeys
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return string 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
