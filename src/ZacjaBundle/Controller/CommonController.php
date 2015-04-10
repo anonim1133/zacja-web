7b4c99803dd62c3f8c1b61f2fbdfe45334798398
anonim1133 <anonim1133@wp.pl>
2015-04-10 17:58:40 +0200

    Added common controller; Moved header to common controller;

---
+++ src/ZacjaBundle/Controller/CommonController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ZacjaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class CommonController extends Controller
+{
+    /**
+     * @Route("/header")
+     * @Template()
+     */
+    public function headerAction(){
+
+	    if ($this->get('security.context')->isGranted('ROLE_USER')){
+		    $user = $this->get('security.token_storage')->getToken()->getUser();
+
+		    $username = $user->getUserName();
+		    $signedin = true;
+	    }else{
+		    $username = 'stranger';
+		    $signedin = false;
+	    }
+
+
+	    return $this->render(
+		    'ZacjaBundle:Common:header.html.twig',
+		    array(
+			    'username' => $username,
+			    'signedIn' => $signedin
+		    )
+	    );
+    }
+
+    /**
+     * @Route("/footer")
+     * @Template()
+     */
+    public function footerAction()
+    {
+        return array(
+                // ...
+            );    }
+
+}
