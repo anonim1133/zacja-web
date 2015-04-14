ff5e75800cdc7710b90bc1ed18f7e6179881469b
anonim1133 <anonim1133@wp.pl>
2015-04-14 12:10:17 +0200

    Fixed showTrainingAction to fully support trainings without gpx - jumps and squats

--- src/ZacjaBundle/Controller/TrainingController.php
+++ src/ZacjaBundle/Controller/TrainingController.php
@@ -12,35 +12,40 @@ class TrainingController extends Controller
      * @Route("/training/{id}", name="training")
      * @Template()
      */
-    public function showTrainingAction($id)
-    {
+    public function showTrainingAction($id){
 	    $training = $this->getDoctrine()->getRepository("ZacjaBundle:Training")->findOneById("$id");
-
-	    $gpx = preg_replace('/\s{2,8}/',' ',$training->getGpx());
 
 	    if($training->getType() === "Biking" || $training->getType() === "Running")
 	        $pattern = '/<trkpt lat="([-+]?[0-9]*\.?[0-9]+)" lon="([-+]?[0-9]*\.?[0-9]+)">\s<ele>([-+]?[0-9]*\.?[0-9]+)<\/ele>\s<time>([0-9]{8}T[0-9]{6})<\/time>\s<speed>([-+]?[0-9]*\.?[0-9]+)<\/speed>\s<\/trkpt>/';
 	    elseif($training->getType() === "Walking")
 		    $pattern = '/<trkpt lat="([-+]?[0-9]*\.?[0-9]+)" lon="([-+]?[0-9]*\.?[0-9]+)">\s<ele>([-+]?[0-9]*\.?[0-9]+)<\/ele>\s<time>([0-9]{8}T[0-9]{6})<\/time>\s<speed>([-+]?[0-9]*\.?[0-9]+)<\/speed>\s<steps>([-+]?[0-9]*\.?[0-9]+)<\/steps>\s<\/trkpt>/';
 
-	    preg_match_all($pattern, $gpx, $gpx);
+	    if(isset($pattern)){
+		    $gpx = preg_replace('/\s{2,8}/',' ',$training->getGpx());
+		    preg_match_all($pattern, $gpx, $gpx);
 
-	    $count = count($gpx[1]);
+		    $training_data = [];
+		    $count = count($gpx[1]);
+		    for($i = 0; $i < $count; $i++){
+			    $training_data[$i]['lat'] = $gpx[1][$i];
+			    $training_data[$i]['lon'] =$gpx[2][$i];
+			    $training_data[$i]['ele'] =$gpx[3][$i];
+			    $training_data[$i]['time'] = strtotime($gpx[4][$i]);
+			    $training_data[$i]['speed'] = round($gpx[5][$i], 2);
+		    }
 
-	    $training_data = [];
-	    for($i = 0; $i < $count; $i++){
-		    $training_data[$i]['lat'] = $gpx[1][$i];
-		    $training_data[$i]['lon'] =$gpx[2][$i];
-		    $training_data[$i]['ele'] =$gpx[3][$i];
-			$training_data[$i]['time'] = strtotime($gpx[4][$i]);
-		    $training_data[$i]['speed'] = round($gpx[5][$i], 2);
+		    return $this->render(
+			    'ZacjaBundle:Training:showTraining.html.twig',
+			    array('training' => $training,
+				    'gpx' => json_encode($training_data))
+		    );
+	    }else{
+		    return $this->render(
+			    'ZacjaBundle:Training:showTraining.html.twig',
+			    array('training' => $training,
+			        'avg' => ($training->getMoves()/$training->getDistance()))
+		    );
 	    }
-
-	    return $this->render(
-		    'ZacjaBundle:Training:showTraining.html.twig',
-		    array('training' => $training,
-			    'gpx' => json_encode($training_data))
-	    );
     }
 
 }
